2b17c931be887d3cd7f03cacf3e3a2f9037b5a73
mohs-in <in.mohsin@outlook.com>
2024-04-29 14:27:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputEl = document.getElementById('input-field');
+const btnEl = document.getElementById("add-button");
+
+btnEl.addEventListener('click', () => {
+    console.log(inputEl.value)
+})
