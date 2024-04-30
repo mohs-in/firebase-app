54cd5b7a6b8d6a252cfa619bd7fbdfad93610684
mohs-in <in.mohsin@outlook.com>
2024-04-30 12:44:52 +0530

    code for deleting items from the list added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: 'https://fir-app-1a7e9-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -11,7 +11,7 @@ const database = getDatabase(app)
 
 const itemsInCart = ref(database, 'items')
 
-let shopList = document.getElementById('shopping-list');
+let shoppingList = document.getElementById('shopping-list');
 const inputEl = document.getElementById('input-field');
 const btnEl = document.getElementById("add-button");
 
@@ -19,21 +19,45 @@ btnEl.addEventListener('click', () => {
     let inputValue = inputEl.value;
     push(itemsInCart, inputValue);
     
-    appendToList(inputValue);
     console.log(`${inputValue} added to database`);
 
     clearInputField();
 })
 
 onValue(itemsInCart, function(snapshot) {
-    let itemsArray = Object.values(snapshot.val())
-    for(let i=0; i< itemsArray.length; i++) {
-        appendToList(itemsArray[i])
+    if(snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val());
+
+        clearShoppingList();
+        
+        for(let i=0; i< itemsArray.length; i++) {
+            let currentItem = itemsArray[i];
+            appendToShoppingList(currentItem)
+        }
+    } else {
+        shoppingList.innerHTML = `Add items to display...`
     }
 })
 
-function appendToList(item) {
-    shopList.innerHTML += `<li>${item}</li>`
+function appendToShoppingList(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `items/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppingList.append(newEl)
+}
+
+function clearShoppingList() {
+    shoppingList.innerHTML = '';
 }
 
 function clearInputField() {
