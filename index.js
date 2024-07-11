0da685046f2ffc1ee327c77944214d8cb17b1cff
mohs-in <in.mohsin@outlook.com>
2024-07-12 01:30:44 +0530

    update variables

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: 'https://fir-app-1a7e9-default-rtdb.europe-west1.firebasedatabase.app/'
+    databaseURL: process.env.DATABASE_URL
 }
 
 const app = initializeApp(appSettings)
